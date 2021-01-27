ecbf4469c6cc31ad48562de0cc90b3280aea24f3
php1301 <19520854@gm.uit.edu.vn>
2021-01-27 15:54:11 +0700

    next.config.js modified

--- apps/mercee/next.config.js
+++ apps/mercee/next.config.js
@@ -1,3 +1,13 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const withNx = require('@nrwl/next/plugins/with-nx');
 
-module.exports = withNx({});
+// module.exports = withNx({});
+
+
+const { withPlugins } = require('next-compose-plugins');
+const withOptimizedImages = require('next-optimized-images');
+
+// next.js configuration
+const nextConfig = {};
+
+module.exports = withPlugins([withOptimizedImages, withNx], nextConfig);
