e320d6960d8337c27819ee22de489cadca7cb911
php1301 <19520854@gm.uit.edu.vn>
2021-01-29 23:10:39 +0700

    custom prop-types added

---
+++ apps/mercee/src/components/utils/prop-types.tsx
@@ -0,0 +1,9 @@
+export const tuple = <T extends string[]>(...args: T) => args;
+
+const buttonVariants = tuple('primary', 'secondary', 'elevation');
+const buttonSizes = tuple('big', 'normal', 'small');
+const counterSizes = tuple('big', 'normal');
+
+export type ButtonVariants = typeof buttonVariants[number];
+export type ButtonSizes = typeof buttonSizes[number];
+export type CounterSizes = typeof counterSizes[number];
