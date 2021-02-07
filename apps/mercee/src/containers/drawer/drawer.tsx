903b95929ef597c96e334ffa15b4019c672fadb9
php1301 <19520854@gm.uit.edu.vn>
2021-02-07 11:42:37 +0700

    cart view of Drawer added

---
+++ apps/mercee/src/containers/drawer/drawer.tsx
@@ -0,0 +1,4 @@
+import React, { useContext } from 'react';
+import { DrawerContext } from 'contexts/drawer/drawer.provider';
+import ProductDetails from './views/product-details';
+import Cart from './views/cart';
