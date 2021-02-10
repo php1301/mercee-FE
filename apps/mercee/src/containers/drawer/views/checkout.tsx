7731a35a6509a11d9950f21c8065e4ba10b78350
php1301 <19520854@gm.uit.edu.vn>
2021-02-10 13:56:17 +0700

    checkout with react-number-format

---
+++ apps/mercee/src/containers/drawer/views/checkout.tsx
@@ -0,0 +1,183 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { useContext, useState } from 'react';
+import NumberFormat from 'react-number-format';
+import { DrawerContext } from 'contexts/drawer/drawer.provider';
+import Scrollbar from 'components/scrollbar';
+import ArrowLeft from 'assets/icons/arrow-left';
+import Input from 'components/input';
+import Button from 'components/button';
+import { useCart } from 'contexts/cart/cart.provider';
+import Textarea from 'components/textarea';
+import {
+  InputBase,
+  TextBoxCommonBase,
+  TextBoxEnable,
+} from 'components/utils/theme';
+import OrderSubmit from './order-submit';
+
+const initialState = {
+  phone_number: '',
+  name: '',
+  email: '',
+  address: '',
+  postal_code: '',
+  suite: '',
+};
+
+export default function Checkout(): React.ReactElement {
+  const { dispatch } = useContext(DrawerContext);
+  const [formData, setFormData] = useState(initialState);
+  const [success, setSuccess] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const { items, calculatePrice, clearCart } = useCart();
+
+  const hideCheckout = () => {
+    dispatch({
+      type: 'TOGGLE_CHECKOUT_VIEW',
+      payload: {
+        showCheckout: false,
+      },
+    });
+  };
+
+  const submitOrder = async () => {
+    const {
+      name, email, address, postal_code, suite, phone_number,
+    } = formData;
+    if (!phone_number.trim()) {
+      setError({
+        field: 'phone_number',
+        message: 'Phone number is required',
+      });
+      return;
+    }
+
+    setLoading(true);
+
+    const res = await fetch('/api/order', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        items,
+        address: `${name} ${address} ${postal_code} ${suite}`,
+        phone_number,
+        email,
+        bill_amount: calculatePrice(),
+      }),
+    });
+    if (res.status === 200) {
+      setSuccess(true);
+      clearCart();
+    } else {
+      setError(true);
+    }
+    setLoading(false);
+  };
+
+  const onChange = (e) => {
+    const { value, name } = e.currentTarget;
+    setFormData({
+      ...formData,
+      // the hien dynamic key
+      [name]: value,
+    });
+  };
+  if (success) {
+    return <OrderSubmit />;
+  }
+
+  return (
+    <div className="flex flex-col w-full h-full">
+      <div className="w-full flex justify-center relative px-30px py-20px">
+        <button
+          className="w-auto h-10 flex items-center justify-center text-gray-500 absolute top-half -mt-20px left-30px transition duration-300 focus:outline-none hover:text-gray-900"
+          onClick={hideCheckout}
+          aria-label="close"
+          type="button"
+        >
+          <ArrowLeft />
+        </button>
+        <h2 className="font-bold text-24px m-0">Checkout</h2>
+      </div>
+
+      <Scrollbar className="checkout-scrollbar flex-grow">
+        <div className="flex flex-col px-30px pt-20px">
+          <div className="flex flex-col mb-45px">
+            <span className="flex font-semibold text-gray-900 text-18px mb-15px">
+              Contact Information
+            </span>
+            <NumberFormat
+              format="+1 (###) ###-####"
+              mask="_"
+              placeholder="Mobile Phone Number"
+              className={`${InputBase} ${TextBoxCommonBase} ${TextBoxEnable} mb-15px`}
+              value={formData.phone_number}
+              onValueChange={({ value }) => setFormData({
+                ...formData,
+                phone_number: value,
+              })}
+            />
+            {error?.field === 'phone_number' && (
+            <p className="text-12px font-semibold text-error pt-10px pl-15px">
+              {error.message}
+            </p>
+            )}
+            <Input
+              placeholder="Email Address"
+              name="email"
+              value={formData.email}
+              onChange={onChange}
+            />
+          </div>
+
+          <div className="flex flex-col">
+            <span className="flex font-semibold text-gray-900 text-18px mb-15px">
+              Shipping Address
+            </span>
+            <Input
+              placeholder="Name"
+              className="mb-10px"
+              name="name"
+              value={formData.name}
+              onChange={onChange}
+            />
+
+            <Textarea
+              placeholder="Enter Address"
+              className="mb-10px"
+              name="address"
+              value={formData.address}
+              onChange={onChange}
+            />
+
+            <div className="flex items-center mb-10px">
+              <Input
+                placeholder="Postal Code"
+                style={{ width: 'calc(50% - 5px)', marginRight: '5px' }}
+                name="postal_code"
+                value={formData.postal_code}
+                onChange={onChange}
+              />
+              <Input
+                placeholder="Apartment, Suite, etc."
+                style={{ width: 'calc(50% - 5px)', marginLeft: '5px' }}
+                name="suite"
+                value={formData.suite}
+                onChange={onChange}
+              />
+            </div>
+          </div>
+        </div>
+      </Scrollbar>
+
+      <div className="flex flex-col p-30px">
+        <Button className="big w-full" onClick={submitOrder} loading={loading}>
+          Order Now
+        </Button>
+      </div>
+    </div>
+  );
+}
