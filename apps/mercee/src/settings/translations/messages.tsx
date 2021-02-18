004a48804d7ae6c3106d4792124e48f97852f5cc
php1301 <19520854@gm.uit.edu.vn>
2021-02-18 13:49:29 +0700

    mutilingual init

---
+++ apps/mercee/src/settings/translations/messages.tsx
@@ -0,0 +1,16 @@
+/* eslint-disable import/prefer-default-export */
+import localEn from './lang/en.json';
+import localAr from './lang/ar.json';
+import localEs from './lang/es.json';
+import localDe from './lang/de.json';
+import localCn from './lang/zh.json';
+import localIl from './lang/he.json';
+
+export const messages = {
+  en: localEn,
+  ar: localAr,
+  es: localEs,
+  de: localDe,
+  zh: localCn,
+  he: localIl,
+};
