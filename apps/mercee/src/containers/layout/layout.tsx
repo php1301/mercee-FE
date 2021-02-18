59eb3f82dcaf5c9f0ea40413b13c565bdc028e7a
php1301 <19520854@gm.uit.edu.vn>
2021-02-18 14:27:32 +0700

    layout modified

--- apps/mercee/src/containers/layout/layout.tsx
+++ apps/mercee/src/containers/layout/layout.tsx
@@ -1,4 +1,5 @@
 import { Drawer, CartDrawer } from 'containers/drawer/drawer';
+import Banner from 'containers/banner/banner';
 import Header from './header';
 import Footer from './footer';
 
@@ -19,7 +20,8 @@ const Layout: React.FC<React.PropsWithChildren<LayoutProps>> = (props) => {
       <Drawer />
       <Header />
       <div className="flex flex-col w-full flex-grow h-full">
-        <div className="pt-90px flex-auto pb-50px md:px-35px">
+        <Banner />
+        <div className="pt-50px flex-auto pb-50px md:px-35px">
           {children}
         </div>
         <Footer />
