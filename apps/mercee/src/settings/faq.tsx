eb15467949efd48010f0ac2bbde2f2a30477569e
php1301 <19520854@gm.uit.edu.vn>
2021-02-21 21:43:25 +0700

    faq data added

---
+++ apps/mercee/src/settings/faq.tsx
@@ -0,0 +1,27 @@
+/* eslint-disable import/prefer-default-export */
+export const faq = [
+  {
+    id: 1,
+    title: 'How to contact with Customer Service?',
+    details:
+        'Our Customer Experience Team is available 6 days a week and we offer various ways to get in contact.Email and Chat. We try to reply quickly, so you need not to wait too long for a response!.',
+  },
+  {
+    id: 2,
+    title: 'App installation failed, how to update system information?',
+    details:
+        'Please read the documentation carefully. We also have some online video tutorials regarding this issue . If the problem remains, Please Open a ticket in the support forum',
+  },
+  {
+    id: 3,
+    title: 'Website reponse taking time, how to improve?',
+    details:
+        'At first, Please check your internet connection . We also have some online video tutorials regarding this issue . If the problem remains, Please Open a ticket in the support forum.',
+  },
+  {
+    id: 4,
+    title: 'How do I create a account?',
+    details:
+        'If you want to open an account for personal use you can do it over the phone or online. Opening an account online should only take a few minutes.',
+  },
+];
